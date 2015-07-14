9724c64ad3de4923e28bcdcf31807eec76d6e752
okam1 <chornenkyy@gmail.com>
2015-07-14 12:59:43 +0300

    Added two classes with description of servlet work

---
+++ src/SimpleGet.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleOne
+ */
+@WebServlet("/SimpleOne")
+public class SimpleGet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SimpleGet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
