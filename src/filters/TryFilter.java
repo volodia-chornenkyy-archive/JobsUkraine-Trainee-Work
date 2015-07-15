76c728b476e32829697bbe34b72263ba17fe3a9f
okam1 <chornenkyy@gmail.com>
2015-07-15 17:37:22 +0300

    add example with forward the request from filter

--- src/filters/TryFilter.java
+++ src/filters/TryFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter("/FilterTest/FilterStartup.jsp")
@@ -15,22 +16,25 @@ public class TryFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		((HttpServletResponse) response).sendRedirect("/Servlets/FilterTest/Error.jsp");
-		
+
+		/*((HttpServletRequest) request).getRequestDispatcher("/FilterTest/Error.jsp")
+				.forward((HttpServletRequest) request, (HttpServletResponse) response);*/
+
 		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
