352dde38bc3c425d0c7deba2b13b6a1c34ba4590
okam1 <chornenkyy@gmail.com>
2015-07-24 14:35:01 +0300

    basic example of spring project

---
+++ Spring/Spring_FirstTry/src/main/java/config/WebConfig.java
@@ -0,0 +1,23 @@
+package main.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("main.java.controllers")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+	}
+
+}
