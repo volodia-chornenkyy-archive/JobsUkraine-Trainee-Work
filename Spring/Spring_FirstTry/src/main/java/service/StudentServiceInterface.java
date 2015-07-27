167e24a7f6d45b2d18906cabcdb41699e860458b
okam1 <chornenkyy@gmail.com>
2015-07-27 11:31:34 +0300

    clean project

--- Spring/Spring_FirstTry/src/main/java/service/StudentServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package main.java.service;
-
-import main.java.beans.Student;
-
-public interface StudentServiceInterface {
-	public Student getRandom();
-	public Student getById(int id);
-	public void save(Student person);
-}
